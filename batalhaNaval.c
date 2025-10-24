776818bdc1ee1b8fa59dc5ee632397663af5c11f
Rafael Ribeiro <163664337+ophisTG@users.noreply.github.com>
2025-10-24 12:49:26 +0000

    Adiciona nível novato - Batalha Naval

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,40 +1,86 @@
 #include <stdio.h>
 
-// Desafio Batalha Naval - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
-// Siga os comentários para implementar cada parte do desafio.
-
-int main() {
-    // Nível Novato - Posicionamento dos Navios
-    // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
-    // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
-    // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
-
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
-
-    // Nível Mestre - Habilidades Especiais com Matrizes
-    // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
-    // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
-    // Sugestão: Exiba o tabuleiro com as áreas afetadas, utilizando 0 para áreas não afetadas e 1 para áreas atingidas.
-
-    // Exemplos de exibição das habilidades:
-    // Exemplo para habilidade em cone:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 1 1 1 1 1
+int main(){
+    // Define tabuleiro, 0 é água...
+    int tabuleiro[10][10] = {0};
+    // Navio Vertical:
+    int navio1[3] = {3, 3, 3};
+    int n1_linha = 4, n1_coluna = 5;
+    // Navio Horizontal:
+    int navio2[3] = {3, 3, 3};
+    int n2_linha = 7, n2_coluna = 2;
+    // Cabeçalho:
+    char linha[10] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
     
-    // Exemplo para habilidade em octaedro:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 0 0 1 0 0
 
-    // Exemplo para habilidade em cruz:
-    // 0 0 1 0 0
-    // 1 1 1 1 1
-    // 0 0 1 0 0
+    // Verifica limite e disponibilidade de casas verticais:
+    int quantia_navios = 3;
+    int casa_disponivel = 1;
+
+    if (n1_linha + quantia_navios > 10){
+        printf("Tamanho vertical do tabuleiro excedido.\n");
+        casa_disponivel = 0;
+    } else {
+        for (int i = 0; i < quantia_navios; i++){
+            if (tabuleiro[n1_linha + i][n1_coluna] != 0){
+                casa_disponivel = 0;
+                break;
+            }
+        }
+    }
+
+    // Adiciona navio vertical: ==> Valor 1 - True, Valor 0 - False.
+    if (casa_disponivel){
+        for (int i = 0; i < quantia_navios; i++){
+            tabuleiro[n1_linha + i][n1_coluna] = navio1[i];
+        };
+    } else {
+        printf("Casa já ocupada.\n");
+    }
+    
+    // Verifica limite e disponibilidade de casas horizontais:
+    casa_disponivel = 1;
+
+    if (n2_coluna + quantia_navios > 10){
+        printf("Tamanho horizontal do tabuleiro excedido.\n");
+        casa_disponivel = 0;
+    } else {
+        for (int i = 0; i < quantia_navios; i++){
+            if (tabuleiro[n2_linha][n2_coluna + i] != 0){
+                casa_disponivel = 0;
+                break;
+            }
+        }
+    }
+
+    // Adiciona navios horizontais: ==> Valor 1 - True, Valor 0 - False.
+    if (casa_disponivel){
+        for (int i = 0; i < quantia_navios; i++){
+            tabuleiro[n2_linha][n2_coluna + i] = navio2[i];
+        }
+    } else {
+        printf("Casa já ocupada.\n");
+    }
+
+    // Imprime cabecalho:
+    printf(" ===============BATALHA NAVAL===============\n");
+    printf("   |");
+    for (int i = 0; i < 10; i++){
+        printf(" %c |", linha[i]);
+    }
+    printf("\n");
+
+    
+
+    // Imprime numerais das casas:
+    for (int vertical = 0; vertical < 10; vertical++){
+        printf("%2d |", vertical + 1);
+        // Imprime tabuleiro:
+        for (int horizontal = 0; horizontal < 10; horizontal++){
+            printf(" %d |", tabuleiro[vertical][horizontal]);
+        }
+        printf("\n");
+    }
 
     return 0;
 }
